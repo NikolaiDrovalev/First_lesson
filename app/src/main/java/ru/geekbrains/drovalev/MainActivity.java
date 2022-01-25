1b4e6060d7a15a77cd342b4deec91a55485a469d
Nikolai <ndrovalev@gmail.com>
2022-01-25 20:17:29 +0300

    1)Создать проект со следующими пользовательскими элементами: TextView, EditText, Button, Switch, CheckBox, ToggleButton. Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты. 2)Создать ещё один макет (если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения.(С помощью Google добавил второе окно"activity_main2" которое вызывается кнопкой "Button" из "activity_main". 3)Добавить в проект элемент CalendarView. Сначала выполнил все задания, потом полез с гит разбираться, поэтому так мало commit.

--- app/src/main/java/ru/geekbrains/drovalev/MainActivity.java
+++ app/src/main/java/ru/geekbrains/drovalev/MainActivity.java
@@ -2,13 +2,32 @@ package ru.geekbrains.drovalev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn_red);
+        btn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_red:
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
